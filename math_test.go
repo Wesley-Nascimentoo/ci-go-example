b1368e47fabb0e8e11457db07620306e3acccdf6
Wesley <wnascx@gmai.com>
2025-07-24 16:54:42 -0300

    fix: rename Sum to sum on SumTest

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func SumTest(t *testing.T) {
-	result := Sum(3, 4)
+	result := sum(3, 4)
 	expected := 7
 	if result != expected {
 		t.Errorf("Sum(3, 4) = %d; want %d", result, expected)
